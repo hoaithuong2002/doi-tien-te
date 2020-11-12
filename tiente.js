117340664e8c34a0884cb7955afa149e98929426
dgth2o2@gmai.com <duonghoaithuong21052002>
2020-11-12 11:19:37 +0700

    nộp bài tiền tệ

--- tiente.js
+++ tiente.js
@@ -1,7 +1,8 @@
 function changeMoney() {
-    let amount = + document.getElementById("amount").value;
-    let from = + document.getElementById('from').value;
-    let to = + document.getElementById('to').value;
-    let result = amount * to / from;
-    document.getElementById("result").innerHTML = result;
+    let amount = +document.getElementById("amount").value;
+    let from = +document.getElementById('from').value;
+    let to = +document.getElementById('to').value;
+    let result = 0;
+    result = amount * to / from;
+    document.getElementById("result").innerHTML = result ;
 }
